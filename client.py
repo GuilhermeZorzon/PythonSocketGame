52f6987b6965d1580f04346070d069b94479932d
GuilhermeZorzon <gui.zorzon@usp.br>
2019-12-08 02:43:49 -0300

    commiting the game

---
+++ client.py
@@ -0,0 +1,175 @@
+import pygame
+from network import Network
+import pickle
+pygame.font.init()
+
+width = 700
+height = 700
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Client")
+
+
+class Button:
+    def __init__(self, text, x, y, color):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 150
+        self.height = 100
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("comicsans", 40)
+        text = font.render(self.text, 1, (255,255,255))
+        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
+
+    def click(self, pos):
+        x1 = pos[0]
+        y1 = pos[1]
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
+
+
+def redrawWindow(win, game, p):
+    win.fill((41,44,52))
+
+    if not(game.connected()):
+        font = pygame.font.SysFont("comicsans", 45)
+        text = font.render("Esperando por jogadores...", 1, (255,255,255), True)
+        win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+    else:
+        font = pygame.font.SysFont("comicsans", 60)
+        text = font.render("Sua jogada", 1, (219,51,36))
+        win.blit(text, (80, 200))
+        
+        font_play = pygame.font.SysFont("comicsans", 40)
+        if(p == 0):
+            text = font_play.render("Jogador " + str(p + 1), 1, (219,51,36))
+            win.blit(text, (130, 150))
+            text = font_play.render("Jogador " + str(p + 2), 1, (219,51,36))
+            win.blit(text, (410, 150))
+        else:
+            text = font_play.render("Jogador " + str(p + 1), 1, (219,51,36))
+            win.blit(text, (130, 150))
+            text = font_play.render("Jogador " + str(p), 1, (219,51,36))
+            win.blit(text, (410, 150))
+
+        text = font.render("Oponente", 1, (219,51,36))
+        win.blit(text, (380, 200))
+
+        move1 = game.get_player_move(0)
+        move2 = game.get_player_move(1)
+        if game.bothWent():
+            text1 = font.render(move1, 1, (255,255,255))
+            text2 = font.render(move2, 1, (255,255,255))
+        else:
+            if game.p1Went and p == 0:
+                text1 = font.render(move1, 1, (255,255,255))
+            elif game.p1Went:
+                text1 = font.render("Escolhido", 1, (255,255,255))
+            else:
+                text1 = font.render("Esperando...", 1, (255,255,255))
+
+            if game.p2Went and p == 1:
+                text2 = font.render(move2, 1, (255,255,255))
+            elif game.p2Went:
+                text2 = font.render("Escolhido", 1, (255,255,255))
+            else:
+                text2 = font.render("Esperando...", 1, (255,255,255))
+
+        if p == 1:
+            win.blit(text2, (65, 350))
+            win.blit(text1, (365, 350))
+        else:
+            win.blit(text1, (65, 350))
+            win.blit(text2, (365, 350))
+
+        for btn in btns:
+            btn.draw(win)
+
+    pygame.display.update()
+
+
+btns = [Button("Pedra", 50, 500, (219,51,36)), Button("Tesoura", 250, 500, (219,51,36)), Button("Papel", 450, 500, (219,51,36))]
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    n = Network()
+    player = int(n.getP())
+    print("Você é o jogador", player)
+
+    while run:
+        clock.tick(60)
+        try:
+            game = n.send("get")
+        except:
+            run = False
+            print("Não conseguimos achar um jogo para você")
+            break
+
+        if game.bothWent():
+            redrawWindow(win, game, player)
+            pygame.time.delay(500)
+            try:
+                game = n.send("reset")
+            except:
+                run = False
+                print("Não conseguimos achar um jogo para você")
+                break
+
+            font = pygame.font.SysFont("comicsans", 90)
+            if (game.winner() == 1 and player == 1) or (game.winner() == 0 and player == 0):
+                text = font.render("Você venceu!", 1, (0,0,0))
+            elif game.winner() == -1:
+                text = font.render("Empatou!", 1, (0,0,0))
+            else:
+                text = font.render("Você perdeu :(", 1, (0, 0, 0))
+
+            win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+            pygame.display.update()
+            pygame.time.delay(2000)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for btn in btns:
+                    if btn.click(pos) and game.connected():
+                        if player == 0:
+                            if not game.p1Went:
+                                n.send(btn.text)
+                        else:
+                            if not game.p2Went:
+                                n.send(btn.text)
+
+        redrawWindow(win, game, player)
+
+def menu_screen():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        win.fill((41,44,52))
+        font = pygame.font.SysFont("comicsans", 45)
+        text = font.render("Clique em qualquer lugar para iniciar o jogo!", 1, (255,255,255))
+        win.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+
+    main()
+
+while True:
+    menu_screen()
